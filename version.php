fc5f0992389becc5e6659e3695f1f7a8841d0eb5
Tim Butler <timb20@gmail.com>
2014-06-17 16:34:47 +1000

    First commit

---
+++ version.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * *************************************************************************
+ * *                  Apply	Enrol   				                      **
+ * *************************************************************************
+ * @copyright   emeneo.com                                                **
+ * @link        emeneo.com                                                **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************
+*/
+/**
+ * Short answer question type version information.
+ *
+ * @package    enrol apply
+ * @copyright  Emeneo {@link http://www.emeneo.com/}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 2014052800;
+$plugin->requires = 2011080100;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'Course Enrol Apply Plugin Version 1.2(build 20131112)';
+$plugin->cron = 180;
